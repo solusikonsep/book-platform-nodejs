c8bda13cad8d0908df9a381291ebd6f8579bb8e4
Alam Wibowo <alamhafidz61@gmail.com>
2025-01-17 07:37:28 +0700

    Server Side Props

--- admin-panel/pages/index.js
+++ admin-panel/pages/index.js
@@ -1,13 +1,38 @@
 import { useEffect, useState } from "react";
 
-export default function Home(){
+export default function Home({ result }){
   const [title, setTitle] = useState("Penulis");
+  const [count, setCount] = useState(0);
 
   useEffect(()=> {
     setTitle("Beranda")
-  }, [])
+  }, []);
+
+  // const getData = async () => {
+  //   console.log("Calling API");
+  //   const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+  //   const result  = await response.json();
+  //   console.log("Ini adalah hasil dari pemanggilan API", result);
+
+
+  // }
+
+  // useEffect(getData, []);
 
   return (
+    <>
     <div>Ini adalah halaman : {title}</div>
+    <div>Ini adalah counter : {count}</div>
+    <div>{result.body}</div>
+    <button onClick={() => setCount(count + 1)}>Increment</button>
+    </>
   )
+}
+
+export async function getServerSideProps(){
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+  const result  = await response.json();
+  return {
+    props: { result }
+  }
 }
